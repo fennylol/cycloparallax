90ed5ef13153a43288f89cc37c67f6af35b13c7a
David Fenneran <dfenneran@gmail.com>
2026-02-01 17:01:28 -0500

    added block inventory UI

--- 1_Tower/the_tower.gd
+++ 1_Tower/the_tower.gd
@@ -8,27 +8,58 @@ class_name  TheTowerNode
 @onready var LevelBlocks : LevelRingNode   = $BlocksSpinnyBit/LevelRing
 @onready var TinyWizard  : TinyWizardNode  = $WizardsSpinnyBit/TinyWizard
 @onready var DollyCamera : DollyCameraNode = $WizardsSpinnyBit/DollyCamera
-@onready var NextBlockUI : Label           = $CanvasLayer/Control/Label
+@onready var NextBlockUI : VBoxContainer   = $CanvasLayer/Control/VBoxContainer
+@onready var BlockInventoryElement = preload("res://5_DollyCam/BlockInventoryElement.tscn")
 signal force_cancel
 var LastSafeSpace := Vector2.ZERO
-var BlockArray : Array = []
+var BlockArray : Array = [LevelRingNode.BlockTypes.PLT_STN]
+var current_level = 2
+# ========================== #
+# associated text and colors #
+# ========================== #
+var item_dict = {
+	LevelRingNode.BlockTypes.PLT_STN:[Color("545454"), "Stone"], 
+	LevelRingNode.BlockTypes.PLT_PLX:[Color("5edb81"), "Parallax"],
+	LevelRingNode.BlockTypes.PLT_GTW:[Color("6eccec"), "Gateway"],
+	LevelRingNode.BlockTypes.PLT_FIR:[Color("ec7380"), "Fire"],
+}
+
 # ================ # 
 # internal utility #
 # ================ #
 func _ready() -> void:
 	LevelBlocks._load_level(2)
+	for i in BlockArray:
+		var new_element = BlockInventoryElement.instantiate()
+		NextBlockUI.add_child(new_element)
+		NextBlockUI.move_child(new_element,0)
+		new_element.get_child(0).color = item_dict.get(i)[0]
+		new_element.get_child(1).text = item_dict.get(i)[1]
 
 func _process(delta: float) -> void:
 	if TheLawsOfTheLand.Paused: return
-	NextBlockUI.text = str(BlockArray.size())
+	#NextBlockUI.text = str(BlockArray.size())
 	BlocksCenter.rotate(Vector3.UP, TinyWizard.WalkingSpeed*delta)
 	DollyCamera.TargetHeight = TinyWizard.position.y
+	
+	if Input.is_action_just_released("reset"): 
+		LevelBlocks._load_level(current_level)
+		BlocksCenter.rotation_degrees.y = 0
+		TinyWizard.position.y = 0.1
+	
 
 func _on_tiny_wizard_pickup_block(type):
 	BlockArray.append(type)
+	var new_element = BlockInventoryElement.instantiate()
+	NextBlockUI.add_child(new_element)
+	NextBlockUI.move_child(new_element,0)
+	new_element.get_child(0).color = item_dict.get(type)[0]
+	new_element.get_child(1).text = item_dict.get(type)[1]
 
 func _on_tiny_wizard_placing_block() -> void:
-	if BlockArray.pop_back() != null: LevelBlocks._place_held_block()
+	if BlockArray.pop_back() != null: 
+		LevelBlocks._place_held_block()
+		NextBlockUI.get_child(0).queue_free()
 
 func _on_tiny_wizard_cancel_placement() -> void:
 	LevelBlocks._cancel_held_block()
